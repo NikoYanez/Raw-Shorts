adb98591080eaad5278e9d8786a5b97ccedd7d80
Nikolas Yanez <yaneznikolas@gmail.com>
2017-01-02 14:00:22 -0500

    added UI for sorting/drag and drop

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
 
+  $( '.colBpicture' ).on('mouseup', function() {
+    $( this ).addClass('movedpic')
+  });
+
+// Let the gallery items be draggable
+  $( '.colBpicture' ).draggable({
+    cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+    revert: "invalid", // when not dropped, the item will revert back to its initial position
+    containment: "document",
+    cursor: "move"
+  });
+
+  // Let the trash be droppable, accepting the gallery items
+  $('#stagedlist').droppable({
+    accept: ".colBpicture"
+  });
+});
